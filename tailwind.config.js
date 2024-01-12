8834eb47369aa9107a0fafdafb3f6c55d45b7fdf
Kractero <kractero@sideroca.com>
2024-01-12 07:27:12 -0700

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/*.ejs", "app.js"],
+  theme: {
+    fontFamily: {
+      'main': ['Inter', 'ui-sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
