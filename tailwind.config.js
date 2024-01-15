33e2e793231335dcbbee2b1b4c545cfb0d9d026f
Kractero <kractero@sideroca.com>
2024-01-14 20:35:22 -0700

    Apply tailwind styles to all views, flip light buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./views/*.ejs", "app.js"],
+  content: ["./views/**/*.ejs", "app.js"],
   theme: {
     fontFamily: {
       'main': ['Inter', 'ui-sans-serif']
