436ddbcdb0b80c568b61b0ca28e5b328c6903498
Kractero <kractero@sideroca.com>
2024-04-20 19:31:44 -0700

    Serve records related stuff while cards are down

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(compression());
-app.use(  helmet.contentSecurityPolicy({
+app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
     scriptSrc: [
@@ -61,7 +61,7 @@ app.get('/', async (req, res) => {
     const sqlQuery = parse(req.query, 50, 1)
     const data = await getOrSetToCache(`/trades?${sqlQuery[0]}${sqlQuery[1]}${sqlQuery[2]}`.toLowerCase(), () => convertTime(db.prepare(sqlQuery[0]).all(...sqlQuery[1])))
     if (req.query.hasOwnProperty('category') && req.query['category'].toLowerCase() === 'all') {
-        delete queryParameters['category'];
+      delete queryParameters['category'];
     }
     const newestRecord = db.prepare('SELECT * FROM records ORDER BY last_updated DESC LIMIT 1;').get();
     sqlQuery[2] = sqlQuery[2].replace('*', "COUNT(*) AS total_count")
@@ -82,7 +82,7 @@ app.get('/tradestotal', async (req, res) => {
   try {
     const queryParameters = req.query;
     if (queryParameters.hasOwnProperty('category') && queryParameters['category'].toLowerCase() === 'all') {
-        delete queryParameters['category'];
+      delete queryParameters['category'];
     }
     const page = queryParameters.page ? parseInt(queryParameters.page) + 1 : 1;
     const sqlQuery = parse(queryParameters, 50, page, "count")
@@ -90,7 +90,7 @@ app.get('/tradestotal', async (req, res) => {
     let tot = Object.keys(req.query).filter(key => validParameters.includes(key)).length > 0 ? await getOrSetToCache(`/tradestotal?${sqlQuery[1]}`.toLowerCase(), () => db.prepare(sqlQuery[2]).all(...sqlQuery[1])) : newestRecord.records
     if (Array.isArray(tot)) tot = tot[0] ? tot[0].total_count : 0
     logger.info(`TRADES TOTAL - /tradestotal ${tot} completed`)
-    res.send({count: tot, update: minutes(newestRecord.last_updated)} )
+    res.send({ count: tot, update: minutes(newestRecord.last_updated) })
   } catch (err) {
     logger.error({
       params: req.query
@@ -109,16 +109,16 @@ app.get('/trades', async (req, res) => {
       res.send(``)
     } else {
       res.send(`
-        ${data.map((entry, index) =>`
+        ${data.map((entry, index) => `
         <tr ${index === data.length - 1 ? `hx-get=/trades?${querystring}&page=${page} hx-trigger=revealed hx-swap=afterend` : ''}>
           <td class="border border-slate-600 p-2 break-words text-xs sm:text-base
             ${entry.category === "c" ? 'text-gray-500' :
-              entry.category === "u" ? 'text-green-500' :
-              entry.category === "r" ? 'text-blue-500' :
+          entry.category === "u" ? 'text-green-500' :
+            entry.category === "r" ? 'text-blue-500' :
               entry.category === "ur" ? 'text-purple-500' :
-              entry.category === "e" ? 'text-yellow-600' :
-              entry.category === "l" ? 'text-yellow-400' : ''
-            }">
+                entry.category === "e" ? 'text-yellow-600' :
+                  entry.category === "l" ? 'text-yellow-400' : ''
+        }">
             <a target="_blank" rel="noreferrer noopener" class="hover:underline"
               href="https://nationstates.net/page=deck/card=${entry.card_id}/season=${entry.season}">
               S${entry.season} ${entry.card_name}
@@ -156,7 +156,7 @@ app.get('/api/trades', limiter, async (req, res) => {
     const newestRecord = db.prepare('SELECT * FROM records ORDER BY last_updated DESC LIMIT 1;').get();
     const tot = Object.keys(req.query).filter(key => validParameters.includes(key)).length > 0 ? await getOrSetToCache(`/${sqlQuery[0]}${sqlQuery[1]}${sqlQuery[2]}/tot`, () => db.prepare(sqlQuery[2]).all(...sqlQuery[1]).length) : newestRecord.records
     logger.info(`API TRADES - ${sqlQuery[0]} / completed`)
-    return res.json({"count": tot, trades: data, "last_updated": newestRecord.last_updated})
+    return res.json({ "count": tot, trades: data, "last_updated": newestRecord.last_updated })
   } catch (err) {
     logger.error({
       params: req.query
@@ -169,10 +169,68 @@ app.get('/health', async (req, res) => {
   res.status(200).send();
 });
 
-app.get ('/docs', function (req, res) {
-  const include = readFileSync (`${__dirname}/views/docs.md`, 'utf8');
-  const html = marked (include);
-  res.render ('docs', {"md": html});
+app.get('/docs', function (req, res) {
+  const include = readFileSync(`${__dirname}/views/docs.md`, 'utf8');
+  const html = marked(include);
+  res.render('docs', { "md": html });
+});
+
+app.get('/download/db', (req, res) => {
+  const filePath = join(__dirname, 'May 11 to April 11.db');
+  if (req.aborted) {
+    logger.info("User aborted download request for db")
+    return;
+  }
+
+  res.download(filePath, (err) => {
+    logger.info("Database downloaded")
+    if (err) {
+      logger.error(err, `An error occured while downloading the database`)
+      res.status(404).send('File not found');
+    }
+  });
+});
+
+app.get('/download/json', (req, res) => {
+  const filePath = join(__dirname, 'trades.json');
+  if (req.aborted) {
+    logger.info("User aborted download request for json")
+    return;
+  }
+  res.download(filePath, (err) => {
+    if (err) {
+      logger.error(err, `An error occured while downloading the json`)
+      res.status(404).send('File not found');
+    }
+  });
+});
+
+function readRecordsFromJson(nation) {
+  const jsonData = JSON.parse(readFileSync("trades.json", "utf-8"));
+  if (jsonData.hasOwnProperty(nation)) {
+    const data = jsonData[nation];
+    return data
+  }
+}
+
+app.get("/records/:nation", async (req, res) => {
+  let { nation } = req.params;
+  nation = nation.toLowerCase().replaceAll(' ', '_')
+  try {
+    const data = await getOrSetToCache(`/records:${nation}`, () => readRecordsFromJson(nation))
+    if (data) {
+      logger.info(`Records request for ${nation} served`)
+      res.json(data);
+    } else {
+      logger.error({
+        params: req.params
+      }, `An error occured on the /records/:nation route: ${nation} not found`)
+      res.status(404).json({ error: "Nation not found" });
+    }
+  } catch (err) {
+    logger.error({ params: req.params }, `An error occurred on the /records/:nation route: ${err}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
